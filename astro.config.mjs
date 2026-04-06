b69eb9c70da6b04070d7bb753de6d829da71cf0c
gitedelormoy <Pascale.pons6@wanadoo.fr>
2026-04-06 19:21:22 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.gitedelormoy.fr',
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
-    sitemap(),
   ],
 });
