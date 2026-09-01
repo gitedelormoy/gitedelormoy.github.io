27a77232e7555b9f95f80f9b5e0da5c549f76b52
gitedelormoy <Pascale.pons6@wanadoo.fr>
2026-09-01 17:58:28 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://gitedelormoy.fr',
+  trailingSlash: 'never', // Force la suppression du slash final sur toutes les URL
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
